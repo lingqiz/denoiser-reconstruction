eed9e3aec1c1b3ac877b82db57a7ab4a2ad864e1
lingqiz <zlqzcc@gmail.com>
2021-06-17 15:16:36 -0400

    bug fix

--- inverse/sampler.py
+++ inverse/sampler.py
@@ -18,26 +18,33 @@ def sample_prior(model, init, h_init=0.01, beta=0.01, sig_end=0.01, stride=5):
 
     # init variables
     n = torch.numel(init)
-    y = init.unsqueeze(0).to(device)    
+    y = init.unsqueeze(0).to(device)
+    sigma = torch.norm(log_grad(y)) / np.sqrt(n)
 
     t = 1
     all_ys = []
     while sigma > sig_end:
+        # update step size
         h = (h_init * t) / (1 + h_init * (t - 1))
 
+        # log prior gradient and estimate noise magnitude
         d = log_grad(y)
-        sigma = torch.norm(d) / torch.sqrt(n)
+        sigma = torch.norm(d) / np.sqrt(n)
 
-        gamma = torch.sqrt((1 - beta * h) ** 2 - (1 - h) ** 2) * sigma
-        noise = torch.randn(size=y.shape(), device=device)
+        # amount of injected noise
+        gamma = np.sqrt((1 - beta * h) ** 2 - (1 - h) ** 2) * sigma
+        noise = torch.randn(size=y.size(), device=device)
         
+        # update image
         y = y + h * d + gamma * noise
 
-        if stride > 0 and t % stride == 0:
-            print('iter %d, sigma %.2f', (t, sigma))
-            all_ys.append(y.cpu().numpy())
+        if stride > 0 and (t - 1) % stride == 0:
+            print('iter %d, sigma %.2f' % (t, sigma.item()))
+            all_ys.append(y.squeeze(0).permute(1, 2, 0).cpu().numpy())
 
         t += 1
 
-    all_ys.append(y)
+    # different convention for numpy vs pytorch images
+    all_ys.append(y.squeeze(0).permute(1, 2, 0).cpu().numpy())
+
     return all_ys
