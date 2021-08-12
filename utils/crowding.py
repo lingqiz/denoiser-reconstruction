e8a469e0fffd471feecfd0b5dccb046d2997c908
lingqiz <zlqzcc@gmail.com>
2021-08-12 13:02:46 -0400

    crowding stim

--- utils/crowding.py
+++ utils/crowding.py
@@ -8,12 +8,12 @@ BACKGROUND = 0.25
 IMG_MAX = 255.0
 
 def letter_image(ltr, ltr_size, im_size):
-    loc = (np.array(im_size) - ltr_size) / 2
-
-    # draw letter
     image = PIL.Image.new('RGB', im_size, color=0)
     draw = PIL.ImageDraw.Draw(image)
     font = PIL.ImageFont.truetype(FONT_PATH, ltr_size)
+
+    # draw letter
+    loc = (np.array(im_size) - ltr_size) / 2
     draw.text(loc, ltr, font=font)
 
     # find the window of the letter
@@ -29,6 +29,22 @@ def letter_image(ltr, ltr_size, im_size):
 
     return (image, stim, index.astype(np.int32))
 
+def letter_array(letters, ltr_size, gap_size, im_size):
+    image = PIL.Image.new('RGB', im_size, color=0)
+    draw = PIL.ImageDraw.Draw(image)
+    font = PIL.ImageFont.truetype('./assets/arial.ttf', ltr_size)
+
+    loc = (im_size[0] - ltr_size) / 2
+    draw.text((loc - ltr_size / 2 - gap_size, loc), letters[0], font=font)
+    draw.text((loc, loc), letters[1], font=font)
+    draw.text((loc + ltr_size / 2 + gap_size, loc), letters[2], font=font)
+
+    # add background and make image / torch stimulus
+    image = np.clip(np.array(image) / IMG_MAX + BACKGROUND, 0, 1)
+    stim = torch.tensor(image.astype(np.float32)).permute([2, 0, 1])
+
+    return image, stim
+
 # base class for setting up an array of templates
 class LetterTask(ABC):
     def __init__(self, ltr, ltr_size, im_size):
@@ -36,9 +52,9 @@ class LetterTask(ABC):
         self.tid = ord(ltr) - ord('A')
 
         # setup an array of templates
-        self.templates, self.indices = self._make_templates(ltr_size, im_size)
+        self.templates, self.indices = self._init_templates(ltr_size, im_size)
 
-    def _make_templates(self, ltr_size, im_size):
+    def _init_templates(self, ltr_size, im_size):
         # produce a set of templates
         templates = []
         indices = []
@@ -69,21 +85,33 @@ class LetterTask(ABC):
 
         return dv, dv[self.tid] - np.mean(dv[mask.astype(bool)])
 
+    def eval_model(self, model):
+        recon = model(self.stimulus)
+        return self.compute_dv(recon)
+
 # class for single letter detection
 class LetterDetection(LetterTask):
     def __init__(self, ltr, ltr_size, im_size):
         super().__init__(ltr, ltr_size, im_size)
 
-        # generate stimulus
-        self.stimulus = self._make_stimulus(ltr_size, im_size)
+        # generate stimulus (static)
+        self.stimulus = self._init_stimulus(ltr_size, im_size)
         self.stim_image = self.stimulus.detach().permute(1, 2, 0).numpy()
 
-    def _make_stimulus(self, ltr_size, im_size):
+    def _init_stimulus(self, ltr_size, im_size):
         return letter_image(self.ltr, ltr_size=ltr_size, im_size=im_size)[1]
 
-    def eval_model(self, model):
-        recon = model(self.stimulus)
-        return self.compute_dv(recon)
-
+# crowding stimulus
 class LetterCrowding(LetterTask):
-    pass
+    def __init__(self, ltr, ltr_size, im_size):
+        super().__init__(ltr, ltr_size, im_size)
+
+        self.ltr_size = ltr_size
+        self.im_size = im_size
+
+    def set_stimulus(self, gap_size, flank):
+        letters = (flank[0], self.ltr, flank[1])
+        self.stim_image, self.stimulus = \
+            letter_array(letters, self.ltr_size, gap_size, self.im_size)
+
+        return self.stim_image, self.stimulus
