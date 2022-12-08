c32914e9d6600c6473164bbf2a2608588167a31e
lingqiz <zlqzcc@gmail.com>
2022-12-07 20:16:30 -0500

    minor update

--- slurm_run.sh
+++ slurm_run.sh
@@ -11,4 +11,4 @@ cd ~/denoiser_recon/
 module load cuda
 source recon/bin/activate
 
-python3 linear.py --n_sample 128 --batch_size 128 --n_epoch 1 --dataset islvrc64 --loss_type MSE
+python3 linear.py --n_sample 128 --batch_size 64 --n_epoch 50 --dataset islvrc64 --loss_type MSE
