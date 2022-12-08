71e7b7db1719d6b5e016c36c84d895be5fc97a9c
lingqiz <zlqzcc@gmail.com>
2022-12-07 21:34:10 -0500

    slurm script with argument

--- slurm_run.sh
+++ slurm_run.sh
@@ -11,4 +11,4 @@ cd ~/denoiser_recon/
 module load cuda
 source recon/bin/activate
 
-python3 linear.py --n_sample 128 --batch_size 64 --n_epoch 50 --dataset islvrc64 --loss_type MSE
+python3 linear.py --n_sample $NSP --batch_size $BSZ --n_epoch $NEP --dataset $DST --loss_type $LST
