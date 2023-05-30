5c2cf8839b258f68a0c24f39c3757025f0c9a414
lingqiz <zlqzcc@gmail.com>
2023-05-30 13:00:10 -0400

    optimization

--- train_run.sh
+++ train_run.sh
@@ -4,24 +4,28 @@
 #SBATCH -p gpu
 #SBATCH --gpus=4
 #SBATCH --constraint=a100
-#SBATCH --mem=768G
+#SBATCH --mem=1000G
 #SBATCH -c 48
-#SBATCH -t 7-00:00:00
+#SBATCH -t 2-00:00:00
 
 cd ~/denoiser_recon/
 module load cuda
 module load python/3.10.8
 source recon/bin/activate
 
+# batch size = 32
+# of epoch = 384
+# AdamW (with weight decay)
+
 echo Training on CelebA
-python3 main.py --mode train --batch_size $BSZ --n_epoch $NEP --opt_index $OPT --ddp True --bias_sd True --scale_image Flase --save_path ./assets/conv3_celeba_tiny.pt --data_path npy_celeba_tiny
+python3 main.py --mode train --batch_size 32 --opt_index 2 --n_epoch 384 --ddp True --bias_sd True --scale_image True --save_path ./assets/conv3_celeba_tiny.pt --data_path npy_celeba_tiny
 
 echo Training on CIFAR
-python3 main.py --mode train --batch_size $BSZ --n_epoch $NEP --opt_index $OPT --ddp True --bias_sd True --scale_image Flase --save_path ./assets/conv3_cifar_10.pt --data_path npy_cifar_10
+python3 main.py --mode train --batch_size 32 --opt_index 2 --n_epoch 384 --ddp True --bias_sd True --scale_image True --save_path ./assets/conv3_cifar_10.pt --data_path npy_cifar_10
 
 echo Training on Pink Noise
-python3 main.py --mode train --batch_size $BSZ --n_epoch $NEP --opt_index $OPT --ddp True --bias_sd True --scale_image Flase --save_path ./assets/conv3_pink_noise.pt --data_path npy_pink_noise
+python3 main.py --mode train --batch_size 32 --opt_index 2 --n_epoch 384 --ddp True --bias_sd True --scale_image True --save_path ./assets/conv3_pink_noise.pt --data_path npy_pink_noise
 
 echo Training on MNIST
-python3 main.py --mode train --batch_size $BSZ --n_epoch $NEP --opt_index $OPT --ddp True --bias_sd True --scale_image Flase --save_path ./assets/conv3_mnist.pt --data_path mnist
+python3 main.py --mode train --batch_size 32 --opt_index 2 --n_epoch 384 --ddp True --bias_sd True --scale_image True --save_path ./assets/conv3_mnist.pt --data_path mnist
 
