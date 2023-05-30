728c120aaec9708e7d6f86222d03c0a11f3266a2
lingqiz <zlqzcc@gmail.com>
2023-05-30 17:59:16 -0400

    bug fix

--- grid.py
+++ grid.py
@@ -21,29 +21,34 @@ print('dataset loaded, size %d' % train_set.size()[0])
 
 # grid search over parameters
 # train with DDP and Multi-GPUs
-if args.ddp:
-    # move dataset to shared memory
-    train_set.share_memory_()
-    test_set.share_memory_()
+def run_search(args):
+    if args.ddp:
+        # move dataset to shared memory
+        train_set.share_memory_()
+        test_set.share_memory_()
 
-    world_size = torch.cuda.device_count()
-    print('model training with %d GPUs' % world_size)
+        world_size = torch.cuda.device_count()
+        print('model training with %d GPUs' % world_size)
 
-    os.environ['MASTER_ADDR'] = 'localhost'
-    os.environ['MASTER_PORT'] = '12355'
+        os.environ['MASTER_ADDR'] = 'localhost'
+        os.environ['MASTER_PORT'] = '12355'
 
-    # list of learning rate and decay rate
-    all_lrs = [1e-3, 1e-2, 1e-1]
-    all_decay = [1e-2, 0.1, 1.0]
-    
-    # iterate through
-    for idx in range(len(all_lrs)):
-        for idy in range(len(all_decay)):            
-            # set parameter values
-            args.lr = all_lrs[idx]
-            args.decay_adam = all_decay[idy]
-    
-            # run network training
-            args.seed = randint(0, 65535)
-            mp.spawn(train_parallel, nprocs=world_size,
-            args=(world_size, train_set, test_set, args))
+        # list of learning rate and decay rate
+        all_lrs = [1e-3, 1e-2, 1e-1]
+        all_decay = [1e-2, 0.1, 1.0]
+        
+        # iterate through
+        for idx in range(len(all_lrs)):
+            for idy in range(len(all_decay)):            
+                # set parameter values
+                args.lr = all_lrs[idx]
+                args.decay_adam = all_decay[idy]
+        
+                # run network training
+                args.seed = randint(0, 65535)
+                mp.spawn(train_parallel, nprocs=world_size,
+                args=(world_size, train_set, test_set, args))
+                
+if __name__ == '__main__':
+    # run grid search
+    run_search(args)
