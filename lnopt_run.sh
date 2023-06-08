c47ddc3c900111f35e8770627469048f9e10495f
lingqiz <zlqzcc@gmail.com>
2023-06-07 21:46:07 -0400

    add argument

--- lnopt_run.sh
+++ lnopt_run.sh
@@ -11,4 +11,4 @@ cd ~/denoiser_recon/
 module load python/3.10.8 cuda cudnn nccl
 source recon/bin/activate
 
-python3 linear.py --n_sample $NSP --dataset $DST --loss_type $LST --recon_method $MTD --batch_size $BSZ --n_epoch $NEP
+python3 linear.py --n_sample $NSP --dataset $DST --model_path $MDP --loss_type $LST --recon_method $MTD --batch_size $BSZ --n_epoch $NEP
