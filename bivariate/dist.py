b68826108fa054f45c6551b3d3f4d3732e435a31
lingqiz <zlqzcc@gmail.com>
2023-09-14 15:39:12 -0400

    gaussian ridge

--- bivariate/dist.py
+++ bivariate/dist.py
@@ -3,6 +3,14 @@ import numpy as np
 # Define bivariate distributions
 
 '''
+2D rotation matrix
+'''
+def rotation(theta):
+    theta = np.radians(theta)
+    return np.array([[np.cos(theta), -np.sin(theta)],
+                     [np.sin(theta), np.cos(theta)]])
+
+'''
 Return samples from a bivariate Normal distribution
 '''
 def normal(mu, sigma, rho, n):
@@ -10,3 +18,21 @@ def normal(mu, sigma, rho, n):
     y = np.random.normal(mu[1], sigma[1], n)
     z = rho * x + np.sqrt(1 - rho**2) * y
     return np.array([x, z]).T
+
+'''
+Sum of two Gaussian ridges
+'''
+def ridge(range=[-1, 1], theta=0, n=1):
+    x = np.random.normal(0, 0.01, n)
+    y = np.random.uniform(range[0], range[1], n)
+    sample_1 = np.array([x, y]).T
+
+    x = np.random.uniform(range[0], range[1], n)
+    y = np.random.normal(0, 0.01, n)
+    sample_2 = np.array([x, y]).T
+
+    sample = np.concatenate((sample_1, sample_2))
+    np.random.shuffle(sample)
+
+    rot_mtx = rotation(theta)
+    return (rot_mtx @ sample.T).T
