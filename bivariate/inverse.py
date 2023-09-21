53104d753e60391467b0e15a124ac9cd0360979a
lingqiz <zlqzcc@gmail.com>
2023-09-21 12:50:24 -0400

    update sample function

--- bivariate/inverse.py
+++ bivariate/inverse.py
@@ -1,14 +1,17 @@
 # Functions for working with bivariate denoisor prior
 import torch, numpy as np
-DIM = 2
+import torch.nn as nn
+import torch.nn.utils.parametrizations as para
+from torch.linalg import vector_norm as vnorm
+N_DIM = 2
 
 # sampling function for bivariate prior
 # 2-D special case for the algorithm in inverse/sampler.py
-def sample(model, device, h=0.1, beta=0.1, end=0.01):
+def sample(model, device, h=0.20, beta=0.20, end=0.025, min_t=4):
     # setup initial values
-    init = torch.rand(DIM) * 2 - 1
+    init = torch.rand(N_DIM) * 2 - 1
     y = init.unsqueeze(0).to(device)
-    sigma = torch.norm(model.score(y)) / np.sqrt(DIM)
+    sigma = torch.norm(model.score(y)) / np.sqrt(N_DIM)
     scale = np.sqrt((1 - beta * h) ** 2 - (1 - h) ** 2)
 
     # record all steps
@@ -20,7 +23,7 @@ def sample(model, device, h=0.1, beta=0.1, end=0.01):
     while sigma > end:
         # compute gradient
         d = model.score(y)
-        sigma = torch.norm(d) / np.sqrt(DIM)
+        sigma = torch.norm(d) / np.sqrt(N_DIM)
 
         # injected noise
         noise = torch.randn(size=y.size(), device=device)
@@ -32,7 +35,10 @@ def sample(model, device, h=0.1, beta=0.1, end=0.01):
         t = t + 1
         all_y.append(y.detach().squeeze().cpu().numpy())
 
+    # set threshold for minimum number of steps
+    if t < min_t:
+        return sample(model, device, h, beta, end, min_t)
+
+    # return the sample trajectory
     return np.array(all_y)
 
-# linear inverse function for bivariate prior and 1-D projection
-# 2-D special case for the algorithm in inverse/orthogonal.py
