9fb3131868a4b616d17954429a07a9cb24b68026
lingqiz <zlqzcc@gmail.com>
2024-01-09 13:49:19 -0500

    minor fix

--- lnopt_run.sh
+++ lnopt_run.sh
@@ -8,7 +8,7 @@
 #SBATCH -t 7-00:00:00
 
 cd ~/denoiser_recon/
-module load python/3.10.8 cuda cudnn nccl
+module load python/3.10 cuda cudnn nccl
 source recon/bin/activate
 
 python3 linear.py --n_sample $NSP --data_path $DST --model_path $MDP --num_layers $NLY --loss_type $LST --recon_method $MTD --batch_size $BSZ --n_epoch $NEP
